b871745ba674f1317d9699b584e753888826d602
jcbondsii <jcbondsii@gmail.com>
2025-12-11 23:53:18 -0600

    Added Player, log sees it, but nothing currently on the screen

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import pygame
 from constants import *
+from player import *
 from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-
-if __name__ == "__main__":
-    main()
+    
     pygame.init()
     # Main game loop
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock=pygame.time.Clock()
     dt = 0
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+   
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -23,6 +23,15 @@ if __name__ == "__main__":
                 pygame.quit()
                 exit()
         screen.fill("black")
+        
         pygame.display.flip()
+        player.draw(screen)
+
         #clock.tick(60)
         dt = clock.tick(60)/1000
+        
+
+
+if __name__ == "__main__":
+    main()
+    
