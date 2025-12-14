633474f3e5c0919495aa74a8210f23e148b54e0a
jcbondsii <jcbondsii@gmail.com>
2025-12-13 23:08:26 -0600

    Added collison for spaceship and asteroids

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
-from logger import log_state
+from logger import log_state, log_event
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -34,18 +35,26 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        screen.fill("black")
-        updatable.update(dt)
         
+        updatable.update(dt)
+        screen.fill("black")
+
         for item in drawable:
             item.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         
         #flip currently needs to go last unless otherwise noted
         pygame.display.flip()
         
         #clock.tick(60)
         dt = clock.tick(60)/1000
-        
+    
+            
 
 
 if __name__ == "__main__":
